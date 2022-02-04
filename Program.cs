0297cf1d3f7ffdcc891dabb6a646016af607d450
syogo <karkinosware@gmail.com>
2022-02-04 14:59:24 +0900

    Update Program.cs
    オーバーフローの検知の実装
    
    演算結果の値がint型で表現できる溶離大きいときは知らせる仕組み。

--- Program.cs
+++ Program.cs
@@ -9,7 +9,9 @@ namespace Karkinosware {
         public FixedPointNumber(){}
         public FixedPointNumber(decimal num) {
             if(num >= (int)Math.Pow(2.0, INTEGER_DIGIT - DIGIT - 1)){
+                Console.ForegroundColor = ConsoleColor.Magenta;
                 Console.WriteLine("入力値は整数部分の上限は65535です");
+                Console.ForegroundColor = ConsoleColor.White;
                 return;
             }
             this.num = decimal.ToInt32(Math.Round(num * (1<<DIGIT)));
@@ -18,74 +20,77 @@ namespace Karkinosware {
         public static FixedPointNumber operator+ (FixedPointNumber x, FixedPointNumber y) {
             FixedPointNumber z = new FixedPointNumber();
             long zNum = (long)x.num + (long)y.num;
-            z.num = (int)zNum;
+            if(zNum > ((long)1<<32)){
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("加算の途中でオーバーフローが発生しました。");
+                Console.ForegroundColor = ConsoleColor.White;
+            } else {
+                z.num = (int)zNum;
+            }
             return z;
         }
         public static FixedPointNumber operator- (FixedPointNumber x, FixedPointNumber y) {
             FixedPointNumber z = new FixedPointNumber();
-            z.num = x.num - y.num;
+            long zNum = (long)x.num - (long)y.num;
+            if(zNum > ((long)1<<32)){
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("減算の途中でオーバーフローが発生しました。");
+                Console.ForegroundColor = ConsoleColor.White;
+            } else {
+                z.num = (int)zNum;
+            }
             return z;
         }
         public static FixedPointNumber operator* (FixedPointNumber x, FixedPointNumber y) {
             FixedPointNumber z = new FixedPointNumber();
             long zNum = (long)x.num * (long)y.num;
-            z.num = (int)(zNum >> DIGIT);
+            zNum = zNum >> DIGIT;
+            if(zNum > ((long)1<<32)){
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("乗算の途中でオーバーフローが発生しました。");
+                Console.ForegroundColor = ConsoleColor.White;
+            } else {
+                z.num = (int)zNum;
+            }
             return z;
         }
         public static FixedPointNumber operator/ (FixedPointNumber x, FixedPointNumber y) {
             FixedPointNumber z = new FixedPointNumber();
             long zNum = (long)x.num / (long)y.num;
-            z.num = (int)(zNum << DIGIT);
+            zNum = zNum << DIGIT;
+            if(zNum > ((long)1<<32)){
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("除算の途中でオーバーフローが発生しました。");
+                Console.ForegroundColor = ConsoleColor.White;
+            } else {
+                z.num = (int)zNum;
+            }
             return z;
         }
 
-        public void PrintValue() {
+        //10進数表記で出力
+        public string ToStringValue() {
             decimal num = this.num;
             num /= (int)Math.Pow(2.0, DIGIT);
-            Console.WriteLine(num);
+            return Convert.ToString(num);
         }
 
-        //2進数表記にする
-        public void BinaryPrintValue(){
-            var str = Convert.ToString(this.num, toBase: 2);
-            Console.WriteLine(str.PadLeft(sizeof(int) * 8, '0'));
+        //2進数表記で出力
+        public string BinaryToStringValue(){
+            var str = Convert.ToString(this.num, toBase:2);
+            str = str.PadLeft(sizeof(int) * 8, '0');
+            return str;
         }        
     }
-}
 
-class Program {
-    static void Main(string[] args) {
-        
-        //入力して、固定小数に変換
-        Karkinosware.FixedPointNumber xFP = new Karkinosware.FixedPointNumber(6000m);
-        Karkinosware.FixedPointNumber yFP = new Karkinosware.FixedPointNumber(10m);
-        
-        //処理結果
-        xFP.PrintValue();
-        xFP.BinaryPrintValue();
-        yFP.PrintValue();
-        yFP.BinaryPrintValue();
-/*
-        Console.ForegroundColor = ConsoleColor.Magenta;
-        Console.WriteLine("///// 加算 /////");
-        Karkinosware.FixedPointNumber addFP = xFP + yFP;
-        addFP.PrintValue();
-
-        Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.WriteLine("///// 減算 /////");
-        Karkinosware.FixedPointNumber subFP = xFP - yFP;
-        subFP.PrintValue();
-
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine("///// 乗算 /////");
-        Karkinosware.FixedPointNumber mulFP = xFP * yFP;
-        mulFP.PrintValue();
-*/
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("///// 除算 /////");
-        Karkinosware.FixedPointNumber divFP = xFP / yFP;
-        divFP.PrintValue();
-
-        Console.ForegroundColor = ConsoleColor.White;
+    class Program {
+        static void Main(string[] args) {
+            FixedPointNumber x = new FixedPointNumber(10000m);
+            FixedPointNumber y = new FixedPointNumber(100m);
+            Console.WriteLine($"{x.ToStringValue()} + {y.ToStringValue()} = {(x + y).ToStringValue()}");
+            Console.WriteLine($"{x.ToStringValue()} - {y.ToStringValue()} = {(x - y).ToStringValue()}");
+            Console.WriteLine($"{x.ToStringValue()} * {y.ToStringValue()} = {(x * y).ToStringValue()}");
+            Console.WriteLine($"{x.ToStringValue()} / {y.ToStringValue()} = {(x / y).ToStringValue()}");
+        }
     }
 }
